3cd1cbe13f81635dc8d93818608ac8ee5b7d4e06
Kyler Spell <KPSPELL@email.sc.edu>
2021-11-22 20:22:36 -0500

    finished p5

--- p5/c1.s
+++ p5/c1.s
@@ -4,6 +4,7 @@ newline: .asciiz "\n"
 
 .text
 main:
+	#get b
 	li $v0, 4
 	la $a0, message
 	syscall
@@ -11,11 +12,41 @@ main:
 	la $a0, newline
 	syscall
 
-	li $v0, 6
+	li $v0, 5
 	syscall
-	
-	li $v0, 2
+
+	move $s1, $v0
+	#get a
+	li $v0, 4
+	la $a0, message
 	syscall
+
+	la $a0, newline
+	syscall
+
+	li $v0, 5
+	syscall
+	move $s2, $v0
+
+	move $t0,$zero 
+	jal bitcheck
+	li $v0, 1
+	move $a0, $t0
+	syscall
+	li $v0, 4
+	la $a0, newline
+	syscall
+	j exit
+
+bitcheck:
+    andi $t2,$s2,1
+    beq $t2,$zero,shift
+    addu $t0,$t0,$s1 
+shift:
+    sll $s1,$s1,1     
+    srl $s2,$s2,1     
+    bne $s2,$zero,bitcheck
+jr $ra
 
 exit:
 	li $v0, 10
